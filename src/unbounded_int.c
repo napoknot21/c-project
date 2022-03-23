0b66af2e683f3ec40b537b0c6aad0fc191d84aa7
napoknot21 <kinbrian5@hotmail.com>
2022-03-23 01:35:49 +0100

    add of commentaries for unbounded_int.c

--- src/unbounded_int.c
+++ src/unbounded_int.c
@@ -5,77 +5,77 @@
 #include "unbounded_int.h"
 
 /**
- * 
- * @param e : 
- * @return unbounded_int nouvelle structure
+ * Convertis un string en type unbounded_int
+ * @param e : le string à convertir
+ * @return unbounded_int la paramètre comme nouvelle structure
  */
 unbounded_int string2unbounded_int (const char *e) {
     return NULL;
 }
 
 /**
- * 
- * @param i 
- * @return unbounded_int 
+ * Convertis un long en type unbounded_int
+ * @param i variable à convertir
+ * @return unbounded_int : paramètre comme nouvelle struct
  */
 unbounded_int ll2unbounded_int (long long i) {
     return NULL;
 }
 
 /**
- * 
- * @param i 
- * @return char* 
+ * Convertis un type unbounded_int en string
+ * @param i struct unbounded_int
+ * @return char* le paramètre sous en format string
  */
 char * unbounded_int2string (unbounded_int i) {
     return NULL;
 }
 
 /**
- * 
- * @param a 
- * @param b 
- * @return int 
+ * Vérifie si la struct a est plus grande que celle de b (ou à l'inverse)
+ * @param a struct unbounded_int
+ * @param b struct unbounded_int
+ * @return int : −1 si a < b; 0 si a == b et 1 sinon  
  */
-int unbounded_int_cmp_unbounded_int (unbounded_int a, long long b) {
+int unbounded_int_cmp_unbounded_int (unbounded_int a, unbounded_int b) {
     return 0;
 }
 
 /**
- * 
- * @param a 
- * @param b 
- * @return int 
+ * Vérifie si la struct a est plus grande que long b (ou à l'inverse)
+ * @param a struct unbounded_int
+ * @param b long 
+ * @return int : −1 si a < b; 0 si a == b et 1 sinon  
  */
-int unbounded_int_cmp_ll (unbounded_int a, unbounded_int b) {
+int unbounded_int_cmp_ll (unbounded_int a, long long b) {
     return 0;
 }
 
 /**
- * 
- * @param a 
- * @param b 
- * @return unbounded_int 
+ * Fais la somme de deux struct unbounded_int
+ * @param a struct unbounded_int
+ * @param b struct unbounded_int
+ * @return la somme sous forme de struct unbounded_int 
  */
 unbounded_int unbounded_int_somme (unbounded_int a, unbounded_int b) {
     return NULL;
 }
 
 /**
- * 
- * @param a 
- * @param b 
- * @return unbounded_int 
+ * Fais la différence des deux struct struct unbounded_int
+ * @param a struct unbounded_int
+ * @param b struct unbounded_int
+ * @return la différence sous forme de struct unbounded_int 
  */
 unbounded_int unbounded_int_difference (unbounded_int a, unbounded_int b) {
     return NULL;
 }
 
 /**
- * 
- * @param a 
- * @param b 
- * @return unbounded_int 
+ * Fais la multiplication de deux structs unbounded_int
+ * @param a struct unbounded_int
+ * @param b struct unbounded_int
+ * @return le produit sous forme de struct unbounded_int 
  */
 unbounded_int unbounded_int_produit (unbounded_int a, unbounded_int b) {
     return NULL;
