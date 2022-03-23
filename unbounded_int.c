072a492b43db6e0839c74bd0d9a179c20c9870b0
napoknot21 <kinbrian5@hotmail.com>
2022-03-23 01:15:35 +0100

    add of unbounded_int.c and unbounded_int.h

---
+++ unbounded_int.c
@@ -0,0 +1,92 @@
+#include<stdio.h>
+#include<stlib.h>
+#include<string.h>
+
+#include "unbounded_int.h"
+
+/**
+ * 
+ * @param e : 
+ * @return unbounded_int nouvelle structure
+ */
+unbounded_int string2unbounded_int (const char *e) {
+    return NULL;
+}
+
+/**
+ * 
+ * @param i 
+ * @return unbounded_int 
+ */
+unbounded_int ll2unbounded_int (long long i) {
+    return NULL;
+}
+
+/**
+ * 
+ * @param i 
+ * @return char* 
+ */
+char * unbounded_int2string (unbounded_int i) {
+    return NULL;
+}
+
+/**
+ * 
+ * @param a 
+ * @param b 
+ * @return int 
+ */
+int unbounded_int_cmp_unbounded_int (unbounded_int a, long long b) {
+    return 0;
+}
+
+/**
+ * 
+ * @param a 
+ * @param b 
+ * @return int 
+ */
+int unbounded_int_cmp_ll (unbounded_int a, unbounded_int b) {
+    return 0;
+}
+
+/**
+ * 
+ * @param a 
+ * @param b 
+ * @return unbounded_int 
+ */
+unbounded_int unbounded_int_somme (unbounded_int a, unbounded_int b) {
+    return NULL;
+}
+
+/**
+ * 
+ * @param a 
+ * @param b 
+ * @return unbounded_int 
+ */
+unbounded_int unbounded_int_difference (unbounded_int a, unbounded_int b) {
+    return NULL;
+}
+
+/**
+ * 
+ * @param a 
+ * @param b 
+ * @return unbounded_int 
+ */
+unbounded_int unbounded_int_produit (unbounded_int a, unbounded_int b) {
+    return NULL;
+}
+
+/**
+ * fonction main
+ * @param argc taille du tableau de argv
+ * @param argv tableau de string
+ * @return int etat de sortie
+ */
+int main (int argc, char *argv[]) {
+    return 0;
+}
---
+++ unbounded_int.h
@@ -0,0 +1,25 @@
+typedef struct chiffre {
+	struct chiffre *suivant;
+	char c; //Pour stocker un chiffre ('0', '1', '2', ..., '9')
+	struct chiffre *precedent;
+} chiffre;
+
+
+typedef struct {
+	char signe; //soit + ou -
+	size_t len; //Taille de la chaine 
+	chiffre *premier; //chiffre le plus élévé
+	chiffre *dernier; //Chiffre des unités
+} unbounded_int;
+
+/**
+ * Prototypes de fonctions (headers)
+ */
+unbounded_int string2unbounded_int (const char *e);
+unbounded_int ll2unbounded_int (long long i);
+char * unbounded_int2string (unbounded_int i);
+int unbounded_int_cmp_unbounded_int (unbounded_int a, long long b);
+int unbounded_int_cmp_ll (unbounded_int a, unbounded_int b);
+unbounded_int unbounded_int_somme (unbounded_int a, unbounded_int b);
+unbounded_int unbounded_int_difference (unbounded_int a, unbounded_int b);
+unbounded_int unbounded_int_produit (unbounded_int a, unbounded_int b);
